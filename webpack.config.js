422b8a92762d0b3381104f5e2724bc7652411799
indepth2018 <13723704903@163.com>
2021-06-11 16:44:09 +0800

    webpack插件丰富

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlPlugin = require('html-webpack-plugin');
+// webpack打包文件体积和依赖关系的可视化
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+// 将样式提取到css文件
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // const nodeExternals = require('webpack-node-externals');
  module.exports = {
     // devtool: 'inline-source-map',
@@ -19,6 +24,22 @@ const HtmlPlugin = require('html-webpack-plugin');
             test: /\.scss$/,
             loader: ['style-loader', 'css-loader', 'sass-loader']
         }, {
+        //     {
+        //     test: /\.css$/,
+        //     exclude: /node_modules/,
+        //     use: ExtractTextPlugin.extract({
+        //         fallback: 'style-loader',
+        //         use: ['css-loader?modules', 'postcss-loader'],
+        //     })
+        // }, {
+        //     test: /\.css$/,
+        //     include: /node_modules/,
+        //     // postcss-loader补全css代码的兼容性前缀
+        //     use: ExtractTextPlugin.extract({
+        //         fallback: 'style-loader',
+        //         use: ['css-loader', 'postcss-loader'],
+        //     })
+        // }, {
             test: /\.(png|svg|jpg|gif)$/,
             loader: 'url-loader',
             options: {
@@ -42,6 +63,8 @@ const HtmlPlugin = require('html-webpack-plugin');
         new webpack.HotModuleReplacementPlugin(),
         new HtmlPlugin({
             template: 'src/index.html'
-        })
+        }),
+        new BundleAnalyzerPlugin({ analyzerPort: 8919 }),
+        // new ExtractTextPlugin({ filename: '[name].[contenthash].css', allChunks: false })
     ]
 }
